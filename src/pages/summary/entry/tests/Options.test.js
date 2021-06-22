32d11a31e7861731157412c19dcc1475a88cfdac
David Levy <lennox.davidlevy@gmail.com>
2021-06-22 11:05:07 -0400

    option scoops working

--- src/pages/summary/entry/tests/Options.test.js
+++ src/pages/summary/entry/tests/Options.test.js
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
 
-test('displays image for each scoop from the server', () => {
+test('displays image for each scoop from the server', async () => {
   render(<Options optionType="scoops" />);
 
   //find images
-  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   //confirm alt text of images

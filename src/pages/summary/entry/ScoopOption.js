a80cd2286f01a844ddfbaacb11820ee607dbc877
David Levy <lennox.davidlevy@gmail.com>
2021-06-22 11:00:43 -0400

    options started

---
+++ src/pages/summary/entry/ScoopOption.js
@@ -0,0 +1,16 @@
+import { NavItem } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+
+const ScoopOption = ({ name, imagePath }) => {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+      <img
+        style={{ width: '75%' }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} scoop`}
+      />
+    </Col>
+  );
+};
+
+export default ScoopOption;
